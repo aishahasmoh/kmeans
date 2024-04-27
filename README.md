a2ffd563a0169e588a8a124647beed1b899858c9
Aisha Mohamed <aishahasmoh@gmail.com>
2024-04-27 18:36:07 -0500

    added read_csv function

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ of a single compute server, to the distributed memory and processors of multip
 compute servers combined together.
 
 ### Kmeans
-
+k-means clustering is an agorithm that finds partitioning of points in a dataset
+to minimize the total variance (sum of quare euclidean distances between each
+datapoint and the centroid.) within groups.
+I implemented clustering of the mall dataset from 
+https://github.com/robertmartin8/udemyML/blob/master/06_clustering/Mall_Customers.csv
+To create a 2D dataset. I used 2 features: spending score and annual income.
 
 ## Setup
 install `mpi` on your machine. For macOS, install the package from this url
@@ -40,4 +45,10 @@ mpicc -o mpi_kmeans mpi_kmeans.c
 To run the program with 4 processes: 
 ```sh
 mpirun -np 4 ./mpi_kmeans
+```
+
+### Sequential Kmeans
+```sh
+gcc -o kmeans kmeans.c
+./kmeans
 ```
--- kmeans.c
+++ kmeans.c
@@ -1,7 +1,7 @@
-#include <mpi.h> //include MPI header to have access to MPI functions.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 struct Point {
     int income;
@@ -22,6 +22,11 @@ struct Point get_spending_score_and_annual_income(char* line) {
     return p;
 }
 
+
+void k_means(struct Point * points, int epochs, int k);
+
+
+
 void read_mall_customers_csv(struct Point * points) {
     // read the csv dataset file
     FILE* stream = fopen("data/Mall_Customers.csv", "r");
@@ -41,18 +46,8 @@ void read_mall_customers_csv(struct Point * points) {
     }
 }
 
-void kMeansClustering(struct Point * points, int epochs, int k);
-
 
 int main( int argc, char **argv ) {
-    int rank, size, k, n;                          
-    MPI_Init( &argc, &argv ); //init the MPI execution environment
-    //Each MPI process is assigned a unique integer ID starting at 0.
-    //Retrieve the local MPI processID in the variable rank.
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    //Retrieve the number of MPI processes running in the variable size. 
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-
     /*Kmeans:
     1. Initialise the clusters: randomly choose k points and assign each data
        point to its nearest cluster point.
@@ -61,34 +56,21 @@ int main( int argc, char **argv ) {
         - Assign each point to the nearest centroid and redefine the cluster
 
      */
+    int k, n;                          
+
     k = 5; // 5 clusters
     n = 200; // Mall Customers dataset contains 200 data points
     struct Point points[n];
 
-    if ( rank == 0 ) {
-        read_mall_customers_csv(points);
-        //for (int j=0; j < n; j++) {
-        //    printf("Income = %d Score = %d\n ", points[j].income, points[j].score);
+    read_mall_customers_csv(points);
+    for (int j=0; j < n; j++) {
+        printf("Income = %d Score = %d\n ", points[j].income, points[j].score);
 
-        //}
-        /* Rank 0 sends an integer to each of the other process ranks */
-        int i;
-        int value = 0;
-        for (i = 1; i < size; i++) {
-            value = value + i;
-            MPI_Send(&value, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
     }
-    else {
-        /* All other process ranks receive one number from Rank 0 */
-        int value;
-        MPI_Status status;
-        MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        printf("Rank %d received value %d\n", rank, value);
-    }
-
-
-    MPI_Finalize( ); //This must be done before the end of the program.
 
     return 0;
 }
+
+
+
+
